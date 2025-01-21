36f552649edd89fdf5ef9171c20360bd44df374b
mawa17 <163126157+mawa17@users.noreply.github.com>
2025-01-21 13:29:39 +0100

    Quiz Proj Creation

---
+++ Quiz-med-JSON-serialisering/QuizApp/WebApp/Program.cs
@@ -0,0 +1,30 @@
+using WebApp.Components;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>();
+
+            app.Run();
+        }
+    }
+}
