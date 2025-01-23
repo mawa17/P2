0ad0661f04fbc4d8004787d9aaa61516889aeadf
mawa17 <163126157+mawa17@users.noreply.github.com>
2025-01-23 14:19:50 +0100

    I AM TIRED v2

--- Quiz-med-JSON-serialisering/QuizApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ Quiz-med-JSON-serialisering/QuizApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa165b0be03bc36af82af3d8241f05c00e920c36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07a3c4d9b0f80376b630ca23d89f680ebdc9a095")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
