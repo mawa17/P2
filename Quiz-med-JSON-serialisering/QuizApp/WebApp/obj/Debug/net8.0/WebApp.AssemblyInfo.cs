07a3c4d9b0f80376b630ca23d89f680ebdc9a095
mawa17 <163126157+mawa17@users.noreply.github.com>
2025-01-23 13:26:55 +0100

    PUSHHH

--- Quiz-med-JSON-serialisering/QuizApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ Quiz-med-JSON-serialisering/QuizApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f552649edd89fdf5ef9171c20360bd44df374b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa165b0be03bc36af82af3d8241f05c00e920c36")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
