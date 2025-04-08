f0dc1f4e380ffc119c0c8bafa0ffc650e2a30895
mawa17 <163126157+mawa17@users.noreply.github.com>
2025-04-08 12:13:52 +0200

    Fixed BasicAuth NO MORE CACHE

--- Quiz/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ Quiz/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4638d10a3af9acd8d50d093a999c2b982ca01c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27063a8a26e47e4c6e1d30dcf7fcf5be1ad3bcce")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
