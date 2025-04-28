6642c448ee3efd17afbb26ace4cecff65fdb7eaf
mawa17 <163126157+mawa17@users.noreply.github.com>
2025-04-28 11:53:57 +0200

    Prod Ready :white_check_mark:

--- Quiz/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ Quiz/BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbaabf78acb3276b21d39422aeaa9cae73d57e14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf84855ae72c7ed2dca3b949f907c48bfaa8dc24")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Quiz/BlazorApp/obj/Release/net9.0/BlazorApp.AssemblyInfo.cs
+++ Quiz/BlazorApp/obj/Release/net9.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0dc1f4e380ffc119c0c8bafa0ffc650e2a30895")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf84855ae72c7ed2dca3b949f907c48bfaa8dc24")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
